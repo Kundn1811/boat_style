e132026a0e37590bf47c76d31e326a361182a1a6
abdus786salam <abdus786salam@gmail.com>
2022-05-05 00:37:58 +0530

    button and some colour added

--- scripts/Earbuds.js
+++ scripts/Earbuds.js
@@ -27,20 +27,26 @@ let h1=document.createElement('h1')
             image.src=el.image
         let detailBox=document.createElement('div')
         let rating=document.createElement('p')
-            rating.innerText=`* ${el.rating}`
+        rating.innerHTML=`<i class="fa-solid fa-star "></i> ${el.rating}`
+            // rating.innerText=`* ${el.rating}`
         let title=document.createElement('h4')
             title.innerText=el.title
         let priceBoxMain=document.createElement('div')
+            priceBoxMain.setAttribute('id','priceBoxMain')
         let priceBox=document.createElement('div')
+            priceBox.setAttribute('id','priceBox')
+        let btn=document.createElement('button')
+            btn.innerText='ADD+'
         let price=document.createElement('p')
-        rating.innerHTML=`<i class="fa-solid fa-star "></i>${el.rating}`
+            price.innerText=`₹ ${el.price}`
+        
        
         let strikedOffPrice=document.createElement('p')
         strikedOffPrice.innerText=` ₹ ${Number(el.price)+30%Number(el.price)} `
         priceBox.append(price,strikedOffPrice)
 
        
-            priceBoxMain.append(priceBox)    
+            priceBoxMain.append(priceBox,btn)    
             detailBox.append(rating,title,priceBoxMain)    
             product.append(image,detailBox)
 
